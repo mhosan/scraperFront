6436616e14b6e5632e70303bf1e0e30e94bb4c45
marcelo <mhosan@gmail.com>
2021-11-18 13:15:21 -0300

    nada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angularFront';
+export class AppComponent implements OnInit {
+  https: any;
+  constructor(private http: HttpClient) { }
+  
+  title = 'app';
+
+  ngOnInit() {
+    console.log('AppComponent.ngOnInit()');
+
+    this.http.get('https://api.countapi.xyz/hit/visits')
+    .subscribe(data => {
+      console.log(data["value"]);
+    }, error => {
+      console.log(error);
+    });
+  }
+  resetearContador() {
+    this.http.get('https://api.countapi.xyz/hit/nonexisting')
+    .subscribe(data => {
+      console.log(data["value"]);
+    }, error => {
+      console.log(error);
+    });
+  }
+
 }
