d1490872e38ba20ca5020a8afbcfac0483079c75
marcelo <mhosan@gmail.com>
2021-11-18 18:24:33 -0300

    nada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,12 @@
-import { Component, OnInit  } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  https: any;
-  constructor(private http: HttpClient) { }
+export class AppComponent {
+  title = 'Argengis';
   
-  title = 'app';
-
-  ngOnInit() {
-    console.log('AppComponent.ngOnInit()');
-
-    this.http.get('https://api.countapi.xyz/hit/visits')
-    .subscribe(data => {
-      console.log(data["value"]);
-    }, error => {
-      console.log(error);
-    });
-  }
-  resetearContador() {
-    this.http.get('https://api.countapi.xyz/hit/nonexisting')
-    .subscribe(data => {
-      console.log(data["value"]);
-    }, error => {
-      console.log(error);
-    });
-  }
 
 }
--- src/app/componentes/footer/footer.component.ts
+++ src/app/componentes/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
-
+  public visitas: number;
+  constructor(private http: HttpClient) { }
   ngOnInit(): void {
+    console.log('AppComponent.ngOnInit()');
+
+    this.http.get('https://api.countapi.xyz/hit/argengis.dx.am/visits')
+    .subscribe(data => {
+      console.log(data["value"]);
+      this.visitas = data["value"];
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
